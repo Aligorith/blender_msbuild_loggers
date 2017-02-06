9de5867c136d5ba0c64e8381721116d74334f017
Joshua Leung <aligorith@gmail.com>
2017-02-07 01:20:59 +1300

    Logger attempt 3 - Change of approach: Only log the stuff we care about
    Currently trying:
    * TargetStarted
    * Warnings
    * Errors
    
    Result:
    * TargetStarted gives out heaps of junk
    * I still haven't been able to trigger any warnings/errors yet!

--- CustomLogger.cs
+++ CustomLogger.cs
@@ -4,29 +4,71 @@ using Microsoft.Build.Framework;
  
 public class CustomLogger: Logger
 {
+	private int warnings = 0;
+	private int errors = 0;
+	
 	public override void Initialize(IEventSource eventSource)
 	{
-		/* Project Finished Events - Only report the ones we care about... */
-		eventSource.ProjectFinished += new ProjectFinishedEventHandler(handleProjectFinished);
+		/* Only register handlers for the events we actually care about...
+		 * If there's something we don't need, don't register a handler
+		 * for it, and it won't get added...
+		 */
+		
+		eventSource.TargetStarted += new TargetStartedEventHandler(handleTargetStarted);
+		eventSource.WarningRaised += new BuildWarningEventHandler(handleWarningRaised);
+		eventSource.ErrorRaised   += new BuildErrorEventHandler(handleErrorRaised);
+		eventSource.BuildFinished += new BuildFinishedEventHandler(handleBuildFinished);
+	}
+	
+	/* Event Handlers ------------------------------------------------ */
+	
+	private void handleTargetStarted(object sender, TargetStartedEventArgs e)
+	{
+		Console.WriteLine(e.Message);
 	}
 	
 	
-	/* Our custom handler for "Project Finished" events */
-	private void handleProjectFinished(object sender, ProjectFinishedEventArgs e)
+	private void handleWarningRaised(object sender, BuildWarningEventArgs e)
 	{
-		// Default Handling - Test
-		Console.BackgroundColor = ConsoleColor.DarkBlue;
+		// XXX: Refine this
+		WriteShadedLine(" ! " + e.Message,
+		                ConsoleColor.Yellow, ConsoleColor.Black);
+		warnings++;
+	}
+	
+	private void handleErrorRaised(object sender, BuildErrorEventArgs e)
+	{
+		// XXX: Refine this
+		WriteShadedLine(" !!! " + e.Message,
+		                ConsoleColor.DarkRed, ConsoleColor.White);
 		
-		//Console.WriteLine(e.Message);
-		//Console.WriteLine("ProjectEnd: " + e.ProjectFile + ", [" + e.Succeeded + "]");
-		
-		/* Filter out all the "ZERO_CHECK ones */
-		if (e.ProjectFile.Contains("ZERO_CHECK.vcxproj") == false &&
-		    e.ProjectFile.Contains("ALL_BUILD.vcxproj") == false)
-		{
-			Console.WriteLine("ProjectEnd: " + e.ProjectFile);
+		errors++;
+	}
+	
+	private void handleBuildFinished(object sender, BuildFinishedEventArgs e)
+	{
+		if (errors == 0) {
+			WriteShadedLine("Build succeeded.", ConsoleColor.DarkGreen, ConsoleColor.White);
 		}
+		else {
+			WriteShadedLine("Build failed.", ConsoleColor.Red, ConsoleColor.Black);
+		}
+		Console.WriteLine( String.Format( "{0} Warning(s)", warnings ) );
+		Console.WriteLine( String.Format( "{0} Error(s)", errors ) );
+	}
+	
+	
+	/* Helper functions ------------------------------------------- */
+	
+	/* Write line which fills the entire row with a solid block of color */
+	private void WriteShadedLine(string message, ConsoleColor bg, ConsoleColor fg)
+	{
+		Console.BackgroundColor = bg;
+		Console.ForegroundColor = fg;
+		
+		Console.WriteLine(message.PadRight(Console.WindowWidth - 1));
 		
 		Console.ResetColor();
 	}
+	
 }
